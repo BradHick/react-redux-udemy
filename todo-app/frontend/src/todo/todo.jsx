7acfc6c2c4944da43a5f982276c763207a31e9af
BradHick <bradleyhrm@gmail.com>
2018-06-02 14:46:43 -0300

    Implementing List and Remove functions

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -18,19 +18,32 @@ export default class Todo extends Component {
 
     this.handleAdd = this.handleAdd.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+
+    this.refresh();
 
   };
+
+  refresh(){
+    axios.get(`${URL}?sort=-createdAt`)
+      .then(resp => this.setState({ ...this.state, description: '', list: resp.data}));
+  }
 
   handleAdd(){
     const description = this.state.description;
     axios.post(URL, { description })
-      .then(resp => console.log('Funcionou!!!'));
-      this.setState({ ...this.state, description: '' });
+      .then(resp => this.refresh());
+      console.log('Funcionou!!!');
       
   };
 
   handleChange(e){
     this.setState({ ...this.state, description: e.target.value });
+  };
+
+  handleRemove(todo){
+    axios.delete(`${URL}/${todo._id}`)
+      .then(resp => this.refresh());
   };
 
   render() {
@@ -39,7 +52,7 @@ export default class Todo extends Component {
         <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
         <TodoForm description={this.state.description} 
         handleAdd={ this.handleAdd } handleChange={this.handleChange}/>
-        <TodoList />
+        <TodoList list = {this.state.list} handleRemove = {this.handleRemove} />
       </div>
     );
   };
