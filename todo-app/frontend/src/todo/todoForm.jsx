3369c1e81a622a4c5c84671521db705908e0e09c
BradHick <bradleyhrm@gmail.com>
2018-06-06 12:26:28 -0300

    Adding shortcut keys

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -3,11 +3,22 @@ import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
 
-export default props => (
-  <div role='form' className='todoForm'>
+export default props => {
+  const keyHandler = (e) => {
+    if(e.key === 'Enter'){
+      e.shiftKey ? props.handleSearch() : props.handleAdd();
+    }else if(e.key === 'Escape') {
+      props.handleClear();
+    };
+  };
+
+  return (
+    <div role='form' className='todoForm'>
     <Grid cols='12 9 10'>
       <input id='description' className='form-control' placeholder='Adicione unma tarefa'
-      value={props.description} onChange={props.handleChange}/>
+        value={props.description} onChange={props.handleChange}
+        onKeyUp={ keyHandler }
+      ></input>
     </Grid>
     <Grid cols='12 3 2'>
       <IconButton style='primary' icon='plus' onClick={ props.handleAdd }/>
@@ -16,4 +27,5 @@ export default props => (
     </Grid>
     
   </div>
-);
+  );
+};
