d4acd7d07551794d96fe786056fe04ad55160ef7
BradHick <bradleyhrm@gmail.com>
2018-06-06 00:15:59 -0300

    Implementing clearForm and improving CSS

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -22,6 +22,7 @@ export default class Todo extends Component {
     this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
     this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.handleClear = this.handleClear.bind(this);
 
     this.refresh();
 
@@ -63,7 +64,11 @@ export default class Todo extends Component {
 
   handleSearch(){
     this.refresh(this.state.description);
-  }
+  };
+
+  handleClear(){
+    this.refresh();
+  };
 
   render() {
     return (
@@ -74,6 +79,7 @@ export default class Todo extends Component {
           handleAdd={ this.handleAdd } 
           handleChange={this.handleChange}
           handleSearch={ this.handleSearch }
+          handleClear={ this.handleClear }
         />
         <TodoList 
           list = {this.state.list} 
