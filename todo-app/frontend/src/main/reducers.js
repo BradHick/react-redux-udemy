a5067d09fa8a30709d3de8e7b5c576d964798621
BradHick <bradleyhrm@gmail.com>
2018-06-06 22:12:59 -0300

    Connecting TodoList with Redux

--- todo-app/frontend/src/main/reducers.js
+++ todo-app/frontend/src/main/reducers.js
@@ -1,20 +1,24 @@
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  todo: () => ({
-    description: 'Ler livro',
-    List: [{
-      _id: 1,
-      description: 'Pagar fatura do cartão',
-      done: true
-    },
-    {
-      _id: 2,
-      description: 'Reunião com a equipe',
-      done: false
-    }
-    ]
-  })
+  todo: () => ({
+    description: 'Ler livro',
+    list: [{
+        _id: 1,
+        description: 'Pagar fatura do cartão',
+        done: true
+      },
+      {
+        _id: 2,
+        description: 'Reunião com a equipe às 10:00',
+        done: false
+      },
+      {
+        _id: 3,
+        description: 'Consulta médicas na terça depois do almoço',
+        done: false
+    }]
+  })
 });
 
 export default rootReducer;
