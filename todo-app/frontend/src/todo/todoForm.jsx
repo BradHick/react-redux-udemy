00100e44f6f5573a7a609df9e3784725413b9c0f
BradHick <bradleyhrm@gmail.com>
2018-06-08 12:24:07 -0300

    Implementing Clear Form

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
-import { add, changeDescription, search } from './todoActions';
+import { add, changeDescription, search, clear } from './todoActions';
 
 class TodoForm extends Component {
   constructor(props) {
@@ -18,16 +18,16 @@ class TodoForm extends Component {
   }
 
   keyHandler(e){
-    const { add, search, description } = this.props;
+    const { add, search, description, clear } = this.props;
     if(e.key === 'Enter'){
       e.shiftKey ? search() : add(description);
     }else if(e.key === 'Escape') {
-      props.handleClear();
+      clear();
     };
   };
 
   render() {
-    const { add, search, description } = this.props;
+    const { add, search, description, clear } = this.props;
     return (
       <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
@@ -39,7 +39,7 @@ class TodoForm extends Component {
         <Grid cols='12 3 2'>
           <IconButton style='primary' icon='plus' onClick={ () => add(description) }/>
           <IconButton style='info' icon='search' onClick={ () => search() }></IconButton>
-          <IconButton style='default' icon='close' onClick={ this.props.handleClear } ></IconButton>
+          <IconButton style='default' icon='close' onClick={ () => clear() } ></IconButton>
         </Grid>
       </div>
     )
@@ -49,6 +49,6 @@ class TodoForm extends Component {
 
 
 const mapStateToProps = state => ({description: state.todo.description});
-const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search, clear }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
