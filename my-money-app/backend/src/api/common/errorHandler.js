ba6d963d92c341a450d1778a3b4d8322e779e2a0
BradHick <bradleyhrm@gmail.com>
2018-06-18 02:01:12 -0300

    Fixing Backend

--- my-money-app/backend/src/api/common/errorHandler.js
+++ my-money-app/backend/src/api/common/errorHandler.js
@@ -1,19 +1,16 @@
-const _ = require('lodash');
-
-
+const _ = require('lodash')
 module.exports = (req, res, next) => {
   const bundle = res.locals.bundle;
-
-  if(bundle.errors){
-    const errors = parseErrors(bundle.errors);
-    res.status(500).json({ errors });
-  } else{
+  if(bundle.errors) {
+  const errors = parseErrors(bundle.errors);
+  res.status(500).json({errors});
+  } else {
     next();
   };
 };
 
-const parseErrors = nodeRestfulErrors => {
+const parseErrors = (nodeRestfulErrors) => {
   const errors = [];
-  _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+  _.forIn(nodeRestfulErrors, error => errors.push(error.message));
   return errors;
 };
--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,14 +1,11 @@
 const express = require('express');
 
-
-module.exports = function(server){
-
-  //Defining base URL for all routes
+module.exports = function(server) {
+  // Definir URL base para todas as rotas
   const router = express.Router();
   server.use('/api', router);
   
-  //Route of Billing Cycle
+  // Rotas de Ciclo de Pagamento
   const BillingCycle = require('../api/billingCycle/billingCycleService');
   BillingCycle.register(router, '/billingCycles');
-  
-};
+}
