45328ae15fb4137f01547de864ac58d81e2ef594
BradHick <bradleyhrm@gmail.com>
2018-06-18 13:01:18 -0300

    Creating first action and reducer

---
+++ my-money-app/frontend/src/common/tab/tabActions.js
@@ -0,0 +1,9 @@
+export function selectTab(tabId){
+  console.log('------------------------------------');
+  console.log(tabId);
+  console.log('------------------------------------');
+  return{
+    type: 'TAB_SELECTED',
+    payload: tabId
+  };
+}
