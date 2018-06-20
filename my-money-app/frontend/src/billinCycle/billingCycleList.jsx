2c1448a7882b8dd4ac879b7abea23da2605f846c
BradHick <bradleyhrm@gmail.com>
2018-06-19 22:53:10 -0300

    BillingCycleList: Showing data on FrontEnd

--- my-money-app/frontend/src/billinCycle/billingCycleList.jsx
+++ my-money-app/frontend/src/billinCycle/billingCycleList.jsx
@@ -5,10 +5,22 @@ import { getList } from './billingCycleAction';
 
 
 class BillingCycleList extends Component {
+
   componentWillMount() {
     this.props.getList();
   };
   
+  renderRows(){
+    const list = this.props.list || [];
+    return list.map(bc => (
+      <tr key={ bc._id }>
+        <td>{ bc.name }</td>
+        <td>{ bc.month }</td>
+        <td>{ bc.year }</td>
+      </tr>
+    ));
+  };
+
   render() {
     console.log(this.props.list);
     return (
@@ -22,7 +34,7 @@ class BillingCycleList extends Component {
             </tr>
           </thead>
           <tbody>
-
+            { this.renderRows() }
           </tbody>
         </table>
       </div>
