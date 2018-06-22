1767728c899b75a5056fe8033d1813a5907c88d6
BradHick <bradleyhrm@gmail.com>
2018-06-21 21:00:15 -0300

    Creating Cancel Button

--- my-money-app/frontend/src/billinCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billinCycle/billingCycleForm.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import { init } from './billingCycleAction';
 import labelAndInput from '../common/form/labelAndInput';
+
 
 class BillingCycleForm extends Component {
   render() {
@@ -20,10 +24,15 @@ class BillingCycleForm extends Component {
         </div>
         <div className='box-footer'>
           <button type='submit' className='btn btn-primary' >Submit</button>
+          <button type='button' className='btn btn-default' onClick={ this.props.init } >Cancelar</button>
         </div>
       </form>  
     );
   };
 };
 
-export default reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm);
+BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm);
+
+const mapDispatchToProps = bindActionCreators({ init }, dispatch);
+
+export default connect(null, mapDispatchToProps)(BillingCycleForm);
