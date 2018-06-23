0ecb972b44c677bc7eed9864ea7a3454690fd5e7
BradHick <bradleyhrm@gmail.com>
2018-06-22 22:24:43 -0300

    Creating and Duplicating Credits

--- my-money-app/frontend/src/billinCycle/creditList.jsx
+++ my-money-app/frontend/src/billinCycle/creditList.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Grid from '../common/layout/grid';
-import { Field } from 'redux-form';
+import { Field, arrayInsert } from 'redux-form';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 
 import Input from '../common/form/input';
 
 class CreditList extends Component{
+
+  add(index, item = {}){
+    if(!this.props.readOnly){
+      this.props.arrayInsert('billingCycleForm', 'credits', index, item);
+    }
+  };
 
   renderRows(){
     const list = this.props.list || [];
@@ -18,7 +27,16 @@ class CreditList extends Component{
           <Field name={`credits[${index}].value`} component={ Input }
             placeholder='Informe o valor' readOnly={ this.props.readOnly }/>
         </td>
-        <td></td>
+        <td>
+          <button type='button' className='btn btn-success'
+            onClick={ () => this.add(index + 1) } >
+            <i className='fa fa-plus'></i>
+          </button>
+          <button type='button' className='btn btn-warning'
+            onClick={ () => this.add(index + 1, item) } >
+            <i className='fa fa-clone'></i>
+          </button>
+        </td>
       </tr>
 
     ));
@@ -34,7 +52,7 @@ class CreditList extends Component{
               <tr>
                 <th>Nome</th>
                 <th>Valor</th>
-                <th>Ações</th>
+                <th className='table-actions'>Ações</th>
               </tr>
             </thead>
             <tbody>
@@ -47,4 +65,6 @@ class CreditList extends Component{
   };
 };
 
-export default CreditList;
+
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert }, dispatch);
+export default connect(null, mapDispatchToProps)(CreditList);
