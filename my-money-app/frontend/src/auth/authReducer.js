6f5591b4cffbc691f776429b6b45ce99e632f820
BradHick <bradleyhrm@gmail.com>
2018-06-24 00:04:08 -0300

    Implementing NavBar with user's data

--- my-money-app/frontend/src/auth/authReducer.js
+++ my-money-app/frontend/src/auth/authReducer.js
@@ -1,7 +1,7 @@
 const userKey = '_mymoney_user';
 
 const INITIAL_STATE = {
-  user: JSON.parse(localStorage.getItem(userKey)),
+  user: {name: 'teste', email: 'teste@teste.com'},//JSON.parse(localStorage.getItem(userKey)),
   validToken: false
 };
 
